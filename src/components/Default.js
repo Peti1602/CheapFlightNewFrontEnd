1548c70250665a3e5a1af341a17f54defa983759
Peter Blasko <blasko.peter.hu@gmail.com>
2019-05-18 19:35:17 +0200

    add default page

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
+import Cheapflight from './Cheapflight';
 
 class Default extends Component {
     render() {
         return (
-            <div className="default-bg">
-                <h3>Page not found</h3>
+            <div className="container col-sm-10 col-md-8 col-lg-6">
+                <div className="row">
+                    <div className="info default-bg t-algin">
+                        <Cheapflight firstname="cheapflight" lastname="info"></Cheapflight>
+                        <h3>Page not found</h3>
+                        <div>
+                            <a href="/">Back to home</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
