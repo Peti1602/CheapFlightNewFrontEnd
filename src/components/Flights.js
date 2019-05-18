35561ee016edfde211c6a3ebc810e27671ba93f2
Peter Blasko <blasko.peter.hu@gmail.com>
2019-05-18 20:38:36 +0200

    create flights page

--- src/components/Flights.js
+++ src/components/Flights.js
@@ -3,8 +3,22 @@ import React, {Component} from 'react';
 class Flights extends Component {
     render() {
         return (
-            <div>
-                <h3>Hello from flights</h3>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="light-blue-bg">
+                        <div>
+                            <div className="container">
+                                <div className="row blue">
+                                    <input id="flyingfrom" className="flyingfrom third-block" type="text" name="" placeholder="flying from" />
+                                    <input id="flyingto" className="flyingto third-block" type="text" name="" placeholder="flying to" />
+                                    <input id="departing"  className="departing third-block" type="text" name="" placeholder="departing" />
+                                    <input id="returning" className="returning third-block" type="text" name="" placeholder="returning" />
+                                    <input id="searchButton" type="button" value="Search" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
