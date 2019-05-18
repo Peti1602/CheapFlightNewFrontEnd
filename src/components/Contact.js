84ddad393afc6463fc22602a3e9f36da8ff60c53
Peter Blasko <blasko.peter.hu@gmail.com>
2019-05-18 20:07:36 +0200

    add some information to Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
+import Cheapflight from "./Cheapflight";
 
 class Contact extends Component {
     render() {
         return (
-            <div>
-                <h3>hello from contact</h3>
+            <div className="container col-sm-10 col-md-8 col-lg-6">
+                <div className="row">
+                    <div className="info default-bg t-algin">
+                        <Cheapflight firstname="" lastname="contact"></Cheapflight>
+                        <h3>Cheap Flight Zrt</h3>
+                        <h4>H-1044 Budapest</h4>
+                        <h4>Nagymező u. 44</h4>
+                        <h3>E-mail: info@cheapflight.com</h3>
+                        <h3>Phone: +36 30 123 45 67</h3>
+                        <div>
+                            <a href="/">Back to home</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
